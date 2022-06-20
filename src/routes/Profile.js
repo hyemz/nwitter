39e46ff53780eeffb9620f29a201157ac2b285aa
윤혜민 <h2m0@swu.ac.kr>
2022-06-21 00:14:46 +0900

    🔨[EDIT] Log out . . (2)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { authService } from 'fbase';
+import { useNavigate } from 'react-router-dom';
 
 export default () => {
+  const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
+    navigate('/');
   };
   return (
     <>
