b8e8875c06bed5ff9516f44c11fb8e5af3611959
윤혜민 <h2m0@swu.ac.kr>
2022-06-28 23:58:43 +0900

    🔨[EDIT] Getting Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { authService } from 'fbase';
+import React, { useEffect } from 'react';
+import { authService, dbService } from 'fbase';
 import { useNavigate } from 'react-router-dom';
+import { collection, where, query, getDocs } from 'firebase/firestore';
 
-export default () => {
+export default ({ userObj }) => {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate('/');
   };
+  const getMyNweets = async () => {
+    const nweets = await query(
+      collection(dbService, 'nweets'),
+      where('creatorId', '==', userObj) // 필터링 하는 방법..
+    );
+    const querySnapshot = await getDocs(nweets);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
